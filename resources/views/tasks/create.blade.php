b77a3bfd902c92165cdd73da3f2f020a456c82b0
tomohiro.isobe <ueda@co.jp>
2018-03-08 00:01:50 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,7 +4,7 @@
 
     <h1>新規タスク作成ページ</h1>
     <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-6 col-md-offset-2 col-md-6 col-lg-offset-3 col-lg-3">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
         {!! Form::model($task, ['route' => 'tasks.store']) !!}
             <div class="form-group">
                 {!! Form::label('status', 'ステータス:') !!}
